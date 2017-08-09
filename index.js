e77f0b43c401454546625b961e2595a131ce7487
Tim <pub@lalatte.com>
2017-08-09 10:34:48 +0800

    Made everything a promise

--- index.js
+++ index.js
@@ -9,55 +9,59 @@ console.log('Loading Lambda function');
 console.log('TOKEN:', process.env.token);
 console.log('UUID:', process.env.uuid);
 
-// const Q = require('q');
-// const UUID = require('uuid');
 const http = require('https');
 
 const GLOBAL_TPLINK_URL = 'wap.tplinkcloud.com';
 
-function httpPost(url, jsonData, token, callback) {
+function httpPostPromise(url, jsonData, token) {
 
-    // console.log('httpPost:', url, JSON.stringify(jsonData), token);
+    return new Promise((resolve, reject) => {
 
-    let path = '';
-    if (token) path = '/?token=' + token;
-    let post_options = {
-        host: url,
-        port: '443',
-        path: path,
-        method: 'POST',
-        headers: {
-            'Accept': '*/*',
-            'Content-Type': 'application/json'
-        }
-    };
-    let post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-            try {
-                const parsedData = JSON.parse(rawData);
-                // console.log(parsedData);
-                callback(null, parsedData);
-            } catch (e) {
-                // console.error(e.message);
-                callback(e);
+        // console.log('httpPostPromise:', url);
+        // console.log('httpPostPromise.jsonData:', JSON.stringify(jsonData));
+        // console.log('httpPostPromise.token:', token);
+
+        let path = '';
+        if (token) path = '/?token=' + token;
+        let post_options = {
+            host: url,
+            port: '443',
+            path: path,
+            method: 'POST',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json'
             }
+        };
+        let post_req = http.request(post_options, function(res) {
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    // console.log(parsedData);
+                    resolve(parsedData);
+                } catch (error) {
+                    // console.error(error.message);
+                    reject(error);
+                }
+            });
+        }).on('error', (error) => {
+            console.error('httpPostPromise:', 'Got error in httpPost', JSON.stringify(e, null, 2));
+            reject(error);
         });
-    }).on('error', (e) => {
-        console.error('Got error in httpPost', JSON.stringify(e, null, 2));
-        callback(e);
-    });
 
-    // post the data
-    post_req.write(JSON.stringify(jsonData));
-    post_req.end();
+        // post the data
+        post_req.write(JSON.stringify(jsonData));
+        post_req.end();
+
+    });
 
 }
 
 // Helper functions
-function CMD_GETTOKEN(user, passwd) { return { method: 'login', params: { appType: 'TPLink Lambda Function', cloudUserName: user, cloudPassword: passwd, terminalUUID: UUID.v4() } } };
+function CMD_GETTOKEN(user, passwd) { return { method: 'login', params: { appType: 'TPLink Lambda Function', cloudUserName: user, cloudPassword: passwd, terminalUUID: UUID } } };
 
 function CMD_GETDEVICES() { return { method: 'getDeviceList' }; }
 
@@ -72,16 +76,9 @@ function CMD_BULB_ON(deviceId) { return CMD_PASSTHROUGH(deviceId, { 'smartlife.i
 function CMD_BULB_OFF(deviceId) { return CMD_PASSTHROUGH(deviceId, { 'smartlife.iot.smartbulb.lightingservice': { 'transition_light_state': { 'on_off': 0 } } }); }
 
 // Functions
-function getToken(user, passwd, callback) {
-    httpPost(GLOBAL_TPLINK_URL, CMD_GETTOKEN(user, passwd), null, (error, response) => {
-        if (error) {
-            // console.error('ERROR in getToken');
-            // console.error(error);
-            return callback(error);
-        } else {
-            // console.log(response);
-            return callback(null, response.result.token);
-        }
+function getTokenPromise(user, passwd) {
+    return httpPostPromise(GLOBAL_TPLINK_URL, CMD_GETTOKEN(user, passwd)).then((response) => {
+        return response.result.token;
     });
 }
 
@@ -92,16 +89,9 @@ function getToken(user, passwd, callback) {
 //     }
 // });
 
-function getDevices(callback) {
-    httpPost(GLOBAL_TPLINK_URL, CMD_GETDEVICES(), process.env.token, (error, response) => {
-        if (error) {
-            // console.error('ERROR in getDevices');
-            // console.error(error);
-            return callback(error);
-        } else {
-            // console.log(response);
-            return callback(null, response.result.deviceList);
-        }
+function getDevicesPromise() {
+    return httpPostPromise(GLOBAL_TPLINK_URL, CMD_GETDEVICES(), process.env.token).then((response) => {
+        return response.result.deviceList;
     });
 }
 
@@ -112,16 +102,9 @@ function getDevices(callback) {
 //     }
 // });
 
-function getSysInfo(deviceUrl, deviceId, callback) {
-    httpPost(deviceUrl, CMD_GETSYSINFO(deviceId), process.env.token, (error, response) => {
-        if (error) {
-            // console.error('ERROR in getSysInfo');
-            // console.error(error);
-            return callback(error);
-        } else {
-            // console.log(response);
-            return callback(null, JSON.parse(response.result.responseData).system.get_sysinfo);
-        }
+function getSysInfoPromise(deviceUrl, deviceId) {
+    return httpPostPromise(deviceUrl, CMD_GETSYSINFO(deviceId), process.env.token).then((response) => {
+        return JSON.parse(response.result.responseData).system.get_sysinfo;
     });
 
     // "smartlife.iot.smartbulb.lightingservice": {
@@ -136,60 +119,37 @@ function getSysInfo(deviceUrl, deviceId, callback) {
 //     }
 // });
 
-function setBulbOnOff(deviceUrl, deviceId, on_off, callback) {
-    httpPost(deviceUrl, CMD_SET_BULB(deviceId, on_off), process.env.token, (error, response) => {
-        if (error) {
-            // console.error('ERROR in setBulbOnOff');
-            // console.error(error);
-            return callback(error);
-        } else {
-            // console.log(response);
-            return callback(null, JSON.parse(response.result.responseData));
-        }
+function setBulbOnOffPromise(deviceUrl, deviceId, on_off) {
+    return httpPostPromise(deviceUrl, CMD_SET_BULB(deviceId, on_off), process.env.token).then((response) => {
+        return JSON.parse(response.result.responseData);
     });
 }
 
 function toggleBulbPromise(deviceUrl, deviceId) {
 
-    return new Promise((resolve, reject) => {
+    console.log('toggleBulbPromise: Toggling device', deviceId, 'on', deviceUrl);
 
-        console.log('Toggling device', deviceId, 'on', deviceUrl);
-    
-        getSysInfo(deviceUrl, deviceId, (error, response) => {
-            if (error) {
-                // console.error(error);
-                reject(error);
-            } else {
-                // console.log(response);
-    
-                // Light state is on: .light_state.on_off
-                let newState = 1 - response.light_state.on_off;
-    
-                setBulbOnOff(deviceUrl, deviceId, newState, (error, response) => {
-                    if (error) {
-                        // console.error(error);
-                        reject(error);
-                    } else {
-                        // console.log(response);
-                        resolve(response);
-                    }
-                });
-            }
-        });
-    
+    return getSysInfoPromise(deviceUrl, deviceId).then((response) => {
+        // console.log(response);
+        // Light state is on: .light_state.on_off
+        let newState = 1 - response.light_state.on_off;
+        return setBulbOnOffPromise(deviceUrl, deviceId, newState);
     });
 }
 
-function toggleBulbOnOff(event, context, callback) {
+function handler(event, context, callback) {
 
-    console.log('Lambda Received event:', JSON.stringify(event, null, 2));
+    console.log('handler: Lambda Received event:', JSON.stringify(event, null, 2));
 
     if (event.devices === undefined) {
-        console.error('ERROR: event needs to have a Device array of url and deviceId');
-        callback(new Error());
+        let error = {
+            message: 'ERROR: event needs to have a Device array of url and deviceId'
+        };
+        console.error(error.message);
+        callback(error);
     } else {
 
-        console.log('Running for', event.devices.length, 'devices');
+        console.log('handler: Toggling', event.devices.length, 'devices');
 
         let promises = [];
 
@@ -208,5 +168,4 @@ function toggleBulbOnOff(event, context, callback) {
 
 }
 
-exports.handler = toggleBulbOnOff;
-
+exports.handler = handler;
